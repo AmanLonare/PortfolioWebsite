52216cffea4667980a16ce459a6d70371f680a66
AmanLonare <amanlonare95@gmail.com>
2022-07-17 00:08:06 +0900

    portfolio website

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 app = Flask(__name__)
 
 
@@ -12,11 +13,20 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+def write_to_csv(data):
+    email = data['email']
+    name = data['subject']
+    message = data['message']
+    with open('database.csv', 'a') as file:
+        csv_writer = csv.writer(file, delimiter=',', quotechar='"', newline='', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, name, message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_csv(data)
         return redirect('/thanku.html')
     else:
         return 'something went wrong'
